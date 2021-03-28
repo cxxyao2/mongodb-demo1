5d079c8e87f857e867b9071197feb8a6aa31b6f1
jenniferyao1996 <jenniferyao1996@hotmail.com>
2021-03-27 20:29:39 -0400

    comment out  logging mongodb connect

--- startup/logging.js
+++ startup/logging.js
@@ -1,34 +1,25 @@
-
-const winston = require('winston');
- require('winston-mongodb');
-require('express-async-errors');
+const winston = require("winston");
+// require('winston-mongodb');
+require("express-async-errors");
 
 module.exports = function () {
   winston.handleExceptions(
-    new winston.transports.Console({ colorize: true, prettyPrint: true}),
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.File({
-      filename: 'uncaughtExceptions.log'
+      filename: "uncaughtExceptions.log",
     })
   );
 
-  winston.add(winston.transports.File, { filename: 'logfile.log'});
-   winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/mongo-exercises'});
+  winston.add(winston.transports.File, { filename: "logfile.log" });
+  // winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/mongo-exercises'});
 
-
-
-  // handle uncaught exceptions 
-  process.on('uncaughtException', (ex) =>  {
-    winston.error(ex.message,ex);
+  // handle uncaught exceptions
+  process.on("uncaughtException", (ex) => {
+    winston.error(ex.message, ex);
     process.exit(1); // 0 = success
   });
 
-
-
-  process.on('unhandledRejection', (ex) =>  {
+  process.on("unhandledRejection", (ex) => {
     throw ex;
   });
-
- 
-
-
-}
+};
